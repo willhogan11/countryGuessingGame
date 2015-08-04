5a9374b435682f2f19529ee48c6d00a14a03524d
Will Hogan <willhogan11@hotmail.com>
2015-08-04 23:55:29 +0100

    Result match to guess method fix

--- src/gmit/FlagGame.java
+++ src/gmit/FlagGame.java
@@ -16,8 +16,10 @@ public class FlagGame {
         int tries = 1;
         int score = 0;
         int answer = 0;
+        String str = "";
 
         try{
+            welcomeMessage();
             startTime = System.currentTimeMillis() / 1000; // Start Timer
             do{
                 fillCountryList(flagNames);
@@ -28,8 +30,8 @@ public class FlagGame {
                         // testPrint(flagNames, selection);
                     // End of testing
                     JOptionPane.showMessageDialog(null, selection);
-                    answerMethod(selection, answer);
-                    score = userGuess(selection, tries, score, answer);
+                    // answerMethod(selection, answer, str);
+                    score = userGuess(selection, tries, score, answer, str);
                     selection.clear(); // Required
                 }while(!flagNames.isEmpty());
             }while(tries != 0);
@@ -39,8 +41,13 @@ public class FlagGame {
         }
     }
 
-    public static int userGuess(List<String> selection, int tries, int score, int answer){
+    public static int userGuess(List<String> selection, int tries, int score, int answer, String str){
         String guess;
+        Random rnd = new Random();
+        int index = rnd.nextInt(selection.size());
+        str = selection.get(index);
+        answer = selection.indexOf(str);
+        JOptionPane.showMessageDialog(null, "Which Country was Number " + (answer+1) + "?");
 
         try{
             guess = JOptionPane.showInputDialog(null, "Please enter you Guess");
@@ -49,7 +56,7 @@ public class FlagGame {
                 tries = 1;
             }else{
                 for(int i=0; i < selection.size(); i++){
-                    if(selection.contains(guess)){
+                    if(str.equalsIgnoreCase(guess)){
                         JOptionPane.showMessageDialog(null, "Correct");
                         score = scoreMethod(score);
                         break;
@@ -58,7 +65,9 @@ public class FlagGame {
                         tries--;
                         if(tries == 0){
                             timeElapsed = System.currentTimeMillis()/1000 - startTime; // End timer
-                            JOptionPane.showMessageDialog(null, "Game Over\n You Scored " + score + "\nIn a time of " + timeElapsed + " seconds");
+                            JOptionPane.showMessageDialog(null, "GAME OVER\nThe Correct Answer was....\n" + str.toUpperCase()
+                                                              + "\nYou Scored " + score
+                                                              + "\nIn a time of " + timeElapsed + " seconds");
                             System.exit(0);
                         }
                         return tries;
@@ -71,16 +80,20 @@ public class FlagGame {
         return score;
     }
 
-    public static void answerMethod(List<String> selection, int answer){
-        Random rnd = new Random();
-        int index = rnd.nextInt(selection.size());
-        String str = selection.get(index);
-        int result = selection.indexOf(str) + 1;
-        JOptionPane.showMessageDialog(null, "Which Country was Number " + result + "?");
-    }
+//    public static void answerMethod(List<String> selection, int answer, String str){
+//        Random rnd = new Random();
+//        int index = rnd.nextInt(selection.size());
+//        str = selection.get(index);
+//        answer = selection.indexOf(str);
+//        JOptionPane.showMessageDialog(null, "Which Country was Number " + answer + "?");
+//    }
 
     public static int scoreMethod(int score){
         return score += 1;
+    }
+
+    public static void welcomeMessage(){
+        JOptionPane.showMessageDialog(null, "WELCOME\nPlease Choose the Correct Country\nfrom each list");
     }
 
     public static void testPrint(List<String> flagNames, List<String> selection){
@@ -117,5 +130,30 @@ public class FlagGame {
         flagNames.add("Sweden");
         flagNames.add("Denmark");
         flagNames.add("Finland");
+        flagNames.add("Andorra");
+        flagNames.add("Armenia");
+        flagNames.add("Azerbaijan");
+        flagNames.add("Belarus");
+        flagNames.add("Bosnia");
+        flagNames.add("Croatia");
+        flagNames.add("Cyprus");
+        flagNames.add("Estonia");
+        flagNames.add("Georgia");
+        flagNames.add("Kazakhstan");
+        flagNames.add("Kosovo");
+        flagNames.add("Latvia");
+        flagNames.add("Liechtenstein");
+        flagNames.add("Lithuania");
+        flagNames.add("Luxembourg");
+        flagNames.add("Macedonia");
+        flagNames.add("Malta");
+        flagNames.add("Montenegro");
+        flagNames.add("Norway");
+        flagNames.add("San Marino");
+        flagNames.add("Serbia");
+        flagNames.add("Slovenia");
+        flagNames.add("Switzerland");
+        flagNames.add("Turkey");
+        flagNames.add("Vatican City");
     }
 }
